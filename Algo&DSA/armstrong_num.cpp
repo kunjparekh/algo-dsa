e4e7228fbb37eed3f6abc5bb615434ebc9ebd1f9
Kunj Parekh <77340676+kunjparekh@users.noreply.github.com>
2021-11-30 15:15:55 +0530

    Update armstrong_num.cpp

--- Algo&DSA/armstrong_num.cpp
+++ Algo&DSA/armstrong_num.cpp
@@ -3,28 +3,25 @@ using namespace std;
 
 int main()
 {
-//     int n,c=0,result=0;
-//     cout<<"Enter The Number: ";
-//     cin>>n;
-//     int x = n;
-//     while(x>0)
-//     {
-//         x=x/10;
-//         c++;
-//     }
-//     x = n;
-//     while(x>0)
-//     {
-//         int r = x%10;
-//         cout<<pow(r,c)<<endl;
-//         result+=pow(r,c);
-//         cout<<result<<endl;
-//         x/=10;
-//     }
+    int n,c=0,result=0;
+    cout<<"Enter The Number: ";
+    cin>>n;
+    int x = n;
+    while(x>0)
+    {
+        x=x/10;
+        c++;
+    }
+    x = n;
+    while(x>0)
+    {
+        int r = x%10;
+        result+=pow(r,c);
+        x/=10;
+    }
     
-//     if(result==n)
-//     cout<<"Amstrong Number\n";
-//     else cout<<"Not Amstrong Number\n";
-cout<<ceil(sqrt(6));
+    if(result==n)
+    cout<<"Amstrong Number\n";
+    else cout<<"Not Amstrong Number\n";
     return 0;
 }
